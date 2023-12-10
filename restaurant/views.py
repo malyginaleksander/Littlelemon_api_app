17787ef9450d431ce9dcd79b4be89fc50eb11117
malyginaleksander <malyginaleksander@icloud.com>
2023-12-10 17:17:33 -0500

    static added

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from .models import Menu
+# from .models import Menu
 
 # Create your views here.
-def menu(request):
-    menu_data = Menu.objects.all()
-    main_data = {'menu': menu_data}
-    return render(request, 'menu.html', {"menu": main_data})
+# def menu(request):
+#     menu_data = Menu.objects.all()
+#     main_data = {'menu': menu_data}
+#     return render(request, 'menu.html', {"menu": main_data})
 
 def index(request):
     return render(request, 'index.html', {})
