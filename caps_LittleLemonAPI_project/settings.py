ea6457d8383e53e190c207b6950350c38803aa42
malyginaleksander <malyginaleksander@icloud.com>
2023-12-11 09:25:24 -0500

    models a update

--- caps_LittleLemonAPI_project/settings.py
+++ caps_LittleLemonAPI_project/settings.py
@@ -75,14 +75,26 @@ WSGI_APPLICATION = 'caps_LittleLemonAPI_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'caps_littlelemon',  
+        'USER': 'root',  
+        'PASSWORD': 'Nobel2023+',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
